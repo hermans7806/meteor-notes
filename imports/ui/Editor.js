6010533afbdf50308be1a61f4f9adce60b71ee68
Herman Sutanto <herman.sutanto@reebonz.com>
2017-09-06 12:10:48 +0800

    Setup Editor to change title & body

--- imports/ui/Editor.js
+++ imports/ui/Editor.js
@@ -2,14 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Session } from 'meteor/session';
+import { Meteor } from 'meteor/meteor';
 
 import { Notes } from '../api/notes';
 
 export class Editor extends React.Component {
+  handleTitleChange(e) {
+    this.props.call('notes.update', this.props.note._id, {
+      title: e.target.value
+    });
+  }
+  handleBodyChange(e) {
+    this.props.call('notes.update', this.props.note._id, {
+      body: e.target.value
+    });
+  }
   render() {
     if (this.props.note) {
       return (
-        <p>We got the note!</p>
+        <div>
+          <input value={this.props.note.title} placeholder="Untitled Note" onChange={ this.handleTitleChange.bind(this) }/>
+          <textarea value={this.props.note.body} placeholder="Your note here" onChange={ this.handleBodyChange.bind(this) }></textarea>
+          <button>Delete Note</button>
+        </div>
       );
     } else {
       return (
@@ -31,6 +46,7 @@ export default createContainer(() => {
 
   return {
     selectedNoteId,
-    note: Notes.findOne(selectedNoteId)
+    note: Notes.findOne(selectedNoteId),
+    call: Meteor.call
   };
 }, Editor);
